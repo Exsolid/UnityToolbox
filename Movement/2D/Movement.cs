cc8b8a06e1b10f77107ad963e56ff747898f9d2c
Samuel Sieberg <ssieberg@exsolid.com>
2022-06-18 11:32:31 +0200

    Adjusted movement merge

--- Movement/2D/Movement.cs
+++ Movement/2D/Movement.cs
@@ -14,21 +14,27 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        input = GetComponent<PlayerInput>(); 
+        input = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         int everyMaskExcept =~ LayerMask.GetMask("Player");
-        grounded = Physics2D.Raycast(bottomTransform.position, transform.up * -1, 0.2f, everyMaskExcept);
+        grounded = Physics2D.Raycast(bottomTransform.position, transform.up * -1, 0.08f, everyMaskExcept);
         move(input.actions[actionName].ReadValue<Vector2>());
-
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
     }
 
     public void move(Vector3 direction)
     {
-        Debug.Log(direction);
+        if (direction.x != 0 || direction.y != 0) ModuleManager.get<LevelEventmanager>().Moving(GetComponent<Rigidbody2D>().velocity);
         GetComponent<Rigidbody2D>().AddForce(new Vector3(direction.x * speed, 0, direction.z * speed));
+    }
+
+    public void moveWithStrength(Vector3 direction, float strength)
+    {
+        if (direction.x != 0 || direction.y != 0) ModuleManager.get<LevelEventmanager>().Moving(GetComponent<Rigidbody2D>().velocity);
+        GetComponent<Rigidbody2D>().AddForce(new Vector3(direction.x * strength, direction.y * strength, direction.z));
     }
 }
