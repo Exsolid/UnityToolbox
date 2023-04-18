2310428f1fa469ad551aa6306fecdd3e542a856e
Samuel Sieberg <ssieberg@exsolid.com>
2023-04-18 15:37:29 +0200

    Adjusted readme

--- Documentation/html/navtreedata.js
+++ Documentation/html/navtreedata.js
@@ -24,42 +24,12 @@
 */
 var NAVTREE =
 [
-  [ "UnityToolbox", "index.html", [
-    [ "UnityToolbox", "md__r_e_a_d_m_e.html", null ],
-    [ "Packages", "namespaces.html", [
-      [ "Package List", "namespaces.html", "namespaces_dup" ]
-    ] ],
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", "functions_dup" ],
-        [ "Functions", "functions_func.html", "functions_func" ],
-        [ "Variables", "functions_vars.html", "functions_vars" ],
-        [ "Enumerations", "functions_enum.html", null ],
-        [ "Properties", "functions_prop.html", null ],
-        [ "Events", "functions_evnt.html", null ]
-      ] ]
-    ] ],
-    [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ],
-      [ "File Members", "globals.html", [
-        [ "All", "globals.html", null ],
-        [ "Enumerations", "globals_enum.html", null ]
-      ] ]
-    ] ]
-  ] ]
+  [ "UnityToolbox Documentation", "index.html", ]
 ];
 
 var NAVTREEINDEX =
 [
-"_audio_mixer_8cs.html",
-"class_dialog_node.html#a8c630e9f7c2e0eaa0f2319f880d60d03",
-"class_localisation_manager.html#adccc52328453d9396cc39e4151f26eca",
-"class_player_event_manager.html",
-"class_terrain_generator.html#abfef4c88396c88af4bd3672f30e3a32a",
-"struct_terrain_generator_1_1_placement_information.html#a99c4764d00d682dcae14fa2b39c7eb6b"
+"index.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- Documentation/html/search/searchdata.js
+++ Documentation/html/search/searchdata.js
@@ -1,45 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "_abcdefghijklmnopqrstuvwxyz",
-  1: "abcdfghilmnopqrstuv",
-  2: "b",
-  3: "abcdfghilmnopqrstuv",
-  4: "abcdefghijlmnoprstuvw",
-  5: "_abcdefghijklmnoprstuvwxyz",
-  6: "abmst",
-  7: "bcefjmnpw",
-  8: "abcdgilmos",
-  9: "o",
-  10: "u"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables",
-  6: "enums",
-  7: "enumvalues",
-  8: "properties",
-  9: "events",
-  10: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Namespaces",
-  3: "Files",
-  4: "Functions",
-  5: "Variables",
-  6: "Enumerations",
-  7: "Enumerator",
-  8: "Properties",
-  9: "Events",
-  10: "Pages"
 };
 
